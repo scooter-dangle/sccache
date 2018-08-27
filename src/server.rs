683c0269169af01a8f5038be284739514375b010
Markus Westerlind <markus.westerlind@distilnetworks.com>
2018-08-27 16:23:32 +0200

    Add authentication to S3 GET requests
    This is configurable through the `SCCACHE_S3_GET_AUTH` environment
    variable. When `"true"`, authentication will be performed in the same
    manner as for PUT requests, allowing completely private buckets to be
    used.

--- src/server.rs
+++ src/server.rs
@@ -1250,7 +1250,7 @@ impl ServerInfo {
 
 enum Frame<R, R1> {
     Body { chunk: Option<R1> },
-    Message { message: R, body: bool, }
+    Message { message: R, body: bool },
 }
 
 struct Body<R> {
